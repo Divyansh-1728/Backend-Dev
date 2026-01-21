11b9d44f848cf1cf3a174fa7a0a9b67d2b889b2b
Divyansh Chaudhary <157566883+Divyansh-1728@users.noreply.github.com>
2026-01-21 14:00:59 +0530

    Delete Assignment_2 directory

--- Assignment_2/Exercise.2/exercise2.js
+++
@@ -1,5 +0,0 @@
-const utils = require('./stringUtils');
-
-console.log(utils.capitalize('hello')); // Hello
-console.log(utils.reverse('world')); // dlrow
-console.log(utils.countVowels('aeiou')); // 5
